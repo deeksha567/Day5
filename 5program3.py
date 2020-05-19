3c36362e675322a06dfed9c15176d8332e3319f8
deeksha567 <65412190+deeksha567@users.noreply.github.com>
2020-05-19 23:52:24 +0530

    Update 5program3.py

--- 5program3.py
+++ 5program3.py
@@ -1,6 +1,14 @@
-n = int(input("Enter the no. of houses: "))
-list1 = list(map(int, input("enter the values in houses separated by spaces: ").split()))
-list2 = list1[::2]
-list3 = list1[1::2]
-value = max(sum(list2), sum(list3))
-print("Maximum stolen value is: " + str(value))
+def max_attain(a):
+    if len(a) == 2:
+        return max(a)
+    if len(a) == 1:
+        return a[0]
+    if len(a) == 3:
+        return max(a[1], a[0] + max_attain(a[2:]))
+    return max(a[1] + max_attain(a[3:]), a[0] + max_attain(a[2:]))
+n=int(input("Enter number of houses: "))
+a=[]
+for i in range(n):
+    c=int(input("Enter value: "))
+    a.append(c)
+print("Max stolen: ",max_attain(a))
